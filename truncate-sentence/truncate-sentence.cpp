00e6a8600a0b7f3eca60e2971bc848ea8662a64e
Lavesh18 <83074446+Lavesh18@users.noreply.github.com>
2021-06-25 12:03:30 +0530

    Time: 8 ms (5.40%), Space: 11.9 MB (5.55%) - LeetHub

---
+++ truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans="";
+        int count=0;
+        for(auto i:s)
+        {
+            if(i==' ')count++;
+            if(count==k)break;
+            ans=ans+i;
+        }
+        return ans;
+    }
+};
+
+    
