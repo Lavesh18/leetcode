4134a440eb9ee01eb74ac9ac909c226856e54416
Lavesh18 <83074446+Lavesh18@users.noreply.github.com>
2021-06-25 11:00:33 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (76.55%) - LeetHub

--- decompress-run-length-encoded-list/decompress-run-length-encoded-list.cpp
+++ decompress-run-length-encoded-list/decompress-run-length-encoded-list.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> decompressRLElist(vector<int>& nums) {
         vector<int> ans;
         int freq,val,i,j;
-        for(i=0;i<nums.size()-1;i+=2)
+        for(i=0;i<nums.size();i+=2)
         {
             freq=nums[i];
             val=nums[i+1];
