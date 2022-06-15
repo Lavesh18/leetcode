83c1dc7fb8363b4415eec810ea029321b4602c91
Lavesh18 <83074446+Lavesh18@users.noreply.github.com>
2022-06-15 07:39:35 +0530

    Added solution - LeetHub

---
+++ 3 sum closest - GFG/3-sum-closest.cpp
@@ -0,0 +1,60 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    int closest3Sum(int A[], int N, int X)
+    {
+        // code here
+        int diff = INT_MAX;
+        int ans = 0;
+        sort(A,A+N);
+        
+        for(int i=0;i<N;i++)
+        {
+            int first = A[i];
+            int start = i+1;
+            int end = N-1;
+            
+            while(start<end)
+            {
+                if(first+A[start]+A[end]==X)
+                    return X;
+                else if(abs(first+A[start]+A[end]-X)<diff)
+                {
+                    diff = abs(first+A[start]+A[end]-X);
+                    ans = first+A[start]+A[end];
+                }
+                else if(first+A[start]+A[end]>X)
+                    end--;
+                else
+                    start++;
+        }
+            }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int Arr[N];
+        for(int i=0;i<N;i++)    
+            cin>>Arr[i];
+        int X;
+        cin>>X;
+        Solution obj;
+        cout<<obj.closest3Sum(Arr, N, X)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
